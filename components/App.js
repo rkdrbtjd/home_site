d10457dc3f0f0191e438a9b617072988627b724d
rkdrbtjd <72647202+rkdrbtjd@users.noreply.github.com>
2025-08-13 16:33:58 +0900

    Update App.js

--- components/App.js
+++ components/App.js
@@ -3,8 +3,10 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainPage from "./components/mainpage";
 import LodgingPage from "./components/LodgingPage";
 import SearchPage from "./components/SearchPage";
-import TransferPage from "./components/TransferPage"; // ★ 추가
+import TransferPage from "./components/TransferPage"; 
 import UploadPage from "./components/UploadPage";
+import DetailLodging from "./components/Detail_Lodging";
+import DetailTransfer from "./components/Detail_Transfer";
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
         <Route path="/transfer" element={<TransferPage />} /> 
         <Route path="/search" element={<SearchPage />} />
         <Route path="/upload" element={<UploadPage />} />
+        <Route path="/detaillodging" element={<DetailLodging />} />
+        <Route path="/detailtransfer" element={<DetailTransfer />} />
       </Routes>
     </Router>
   );
